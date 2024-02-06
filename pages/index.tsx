15730aa332c1395e05042cec0f7e35b92be09e21
Ines Alexandra Rodriguez <alexandra.rodriguez@soytul.com>
2024-02-06 16:25:35 -0300

    feat: first component and fix hydratation issue importing Head

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,20 @@
+import { RandomFox } from '@/components/RandomFox';
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <>
-      <div>
-        <head>
-          <title>App React y Typescript</title>
-          <link rel='shortcut icon' href='favicon.ico' type='image/x-icon' />
-        </head>
-      </div>
+    <div>
+      <Head>
+        <title>React app</title>
+      </Head>
       <main>
         <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+        <RandomFox />
       </main>
       <footer></footer>
-    </>
+    </div>
   );
 }
