7596da47418667976159ee68f0b49f4414514374
Ines Alexandra Rodriguez <alexandra.rodriguez@soytul.com>
2024-02-06 17:02:07 -0300

    feat: hooks with custom type and generate random uuid

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,16 @@ import Head from 'next/head';
 import { useState } from 'react';
 
 const random = (): number => Math.floor(Math.random() * 123) + 1;
+const generateId = (): string => {
+  return Math.random().toString(36).substring(2, 15);
+};
+type ImageItem = { id: string; url: string };
 export default function Home() {
-  const [images, setImages] = useState<string[]>([
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`,
-    `https://randomfox.ca/images/${random()}.jpg`
+  const [images, setImages] = useState<ImageItem[]>([
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` },
+    { id: generateId(), url: `https://randomfox.ca/images/${random()}.jpg` }
   ]);
   return (
     <div>
@@ -17,9 +21,9 @@ export default function Home() {
       </Head>
       <main>
         <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-        {images.map((image, index) => (
-          <div key={index} className='p-4'>
-            <RandomFox image={image} />
+        {images.map(({ url, id }) => (
+          <div key={id} className='p-4'>
+            <RandomFox image={url} />
           </div>
         ))}
       </main>
